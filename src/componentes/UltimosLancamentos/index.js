62019751b14f328e8f59d3125fa58ad151a8c44a
Lucas Rodrigues <lucas.r.c2010@hotmail.com>
2025-08-05 20:57:52 -0300

    update

--- src/componentes/UltimosLancamentos/index.js
+++ src/componentes/UltimosLancamentos/index.js
@@ -1,4 +1,5 @@
 import livros  from './dadosUltimosLancamentos';
+import {Titulo} from '../Titulo';
 import styled from 'styled-components';
 
 const UltimosLancamentosContainer = styled.section`
@@ -6,15 +7,6 @@ const UltimosLancamentosContainer = styled.section`
     padding-bottom: 20px;
     display: flex;
     flex-direction: column;
-`
-const Titulo = styled.h2 `
-    width: 100%;
-    padding: 30px 0;
-    background-color: #FFFFFF;
-    color: #EB9B00;
-    font-size: 36px;
-    text-align: center;
-    margin: 0;
 `
 
 const NovosLivrosContainer = styled.div`
@@ -28,13 +20,16 @@ const NovosLivrosContainer = styled.div`
 function UltimosLancamentos() {
     return (
         <UltimosLancamentosContainer>
-        <Titulo>ÚLTIMOS LANÇAMENTOS</Titulo>
-        <NovosLivrosContainer>
-            {livros.map((livro) => (
-                <img key={livro.id} src={livro.src} alt={livro.nome} />
-            ))}
-        </NovosLivrosContainer>
-        </UltimosLancamentosContainer>
+            <Titulo 
+                cor="#EB9B00" 
+                tamanhoFonte="36px" 
+            >ÚLTIMOS LANÇAMENTOS</Titulo>
+            <NovosLivrosContainer>
+                {livros.map((livro) => (
+                    <img key={livro.id} src={livro.src} alt={livro.nome} />
+                ))}
+            </NovosLivrosContainer>
+            </UltimosLancamentosContainer>
     );
 }
 
